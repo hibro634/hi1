f53cbe96161768f668a28944ef6bea78d19c0075
hibro634 <156399043+hibro634@users.noreply.github.com>
2024-01-12 10:51:03 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -4,7 +4,7 @@ st.header('Supra')
 st.write('Supra')
 
 # Use a valid image URL
-image_url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peakpx.com%2Fen%2Fsearch%3Fq%3Dtoyota%2Bsupra%2Btwin%2Bturbo&psig=AOvVaw3ghS5xdzWNPrpYVVZVkO7d&ust=1705123038526000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMD-0buM14MDFQAAAAAdAAAAABAD'
+image_url = 'https://i.pinimg.com/736x/8e/eb/e1/8eebe11cec68e1d4732e71e3f0411cc2.jpg'
 
 # Display the image with a caption
 st.image(image_url, caption='Toyota Supra')
