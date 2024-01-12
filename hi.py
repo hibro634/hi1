fc859ccd0285a0975ad923c5bcd4d053ec8aa864
hibro634 <156399043+hibro634@users.noreply.github.com>
2024-01-12 10:38:23 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,4 +1,4 @@
-import streamlitas st
+import streamlit as st
 st.header('Naruto and hinata')
 st.write('Naruto and hinata')
 st.image('https://medium.com/@lazyanukul/why-hinata-love-naruto-42b32e402c3c',caption='Naruto and hinata')
