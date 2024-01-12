1be5f8404d4341eb36518ab3303b628347defee5
hibro634 <156399043+hibro634@users.noreply.github.com>
2024-01-12 10:56:01 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -4,7 +4,7 @@ st.header('Supra Video')
 st.write('Watch the Toyota Supra in action!')
 
 # Use a valid video URL
-video_url = 'https://youtu.be/96fWYzQK4XA?si=_QZELkSNquRzJpD_'
+video_url = 'https://youtu.be/ZtkgNfC9exI?si=Nexa5W62_TqvD4PV'
 
 # Display the video
 st.video(video_url)
