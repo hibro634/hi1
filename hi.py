58df14449980e298bd08bab5724d55d5383c06cb
hibro634 <156399043+hibro634@users.noreply.github.com>
2024-01-12 10:47:49 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -4,7 +4,7 @@ st.header('Supra')
 st.write('Supra')
 
 # Use a valid image URL
-image_url = 'https://www.ronbrooks.co.uk/wp-content/uploads/2020/03/Toyota-Supra-1.jpg'
+image_url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peakpx.com%2Fen%2Fsearch%3Fq%3Dtoyota%2Bsupra%2Btwin%2Bturbo&psig=AOvVaw3ghS5xdzWNPrpYVVZVkO7d&ust=1705123038526000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMD-0buM14MDFQAAAAAdAAAAABAD'
 
 # Display the image with a caption
 st.image(image_url, caption='Toyota Supra')
